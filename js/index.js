268944820b78e4fd17dfa1f56329a5d78bccc3ac
merestnora <merestnora@outlook.com>
2018-12-08 11:38:43 -0500

    float window

--- js/index.js
+++ js/index.js
@@ -27,6 +27,10 @@ $("#body_img").mapster({
     ]
 });
 
-function showFloatWindow(organ){
-    $("#"+organ).attr("border", "solid");
+function clickShowDiv(obj){
+    var position_x = obj.getBoundingClientRect().left+document.body.scrollLeft+135;
+    var position_y = obj.getBoundingClientRect().top+document.body.scrollTop;
+    $(obj).next().css("left", position_x);
+    $(obj).next().css("top", position_y);
+    $(obj).next().css("display", "block");
 }
--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,1 @@
-function clickShowDiv(obj) { 
-	var position_x = obj.getBoundingClientRect().left+document.body.scrollLeft+135; 
-	var position_y = obj.getBoundingClientRect().top+document.body.scrollTop; 
-	$(obj).next().css("left", position_x); 
-	$(obj).next().css("top", position_y); 
-	$(obj).next().css("display", "block"); 
-}
+
