b7ec6c6a4f482c850010a4db4e71f5a3e6775c05
Anmolpreet Kandola <akandola97@gmail.com>
2018-12-08 22:43:33 -0500

    add hard coded smptom gifs

--- js/script.js
+++ js/script.js
@@ -23,6 +23,328 @@ function getNormalAbdomen(){
 	});
 }
 
+function getAbdomenHunger(){
+	var ts = new Date().getTime();
+	var hash = md5(ts+api_key).toString();
+	var url = 'https://api.giphy.com/v1/gifs';
+	var id1 = 'JE1YlMbVIPxUA';
+	$.getJSON(url,{
+		ts:ts,
+		apikey:api_key,
+		hash:hash,
+		ids:id1,
+		limit:10
+	})
+	.done(function(data){
+		console.log(data);
+		// for(var i=0;i<10;i++){
+		// 	$('#result').append(data.data.results[i]);
+		// }
+	})
+	.fail(function(err){
+		console.log(err);
+	});
+}
+
+function getAbdomenDehydration(){
+	var ts = new Date().getTime();
+	var hash = md5(ts+api_key).toString();
+	var url = 'https://api.giphy.com/v1/gifs';
+	var id1 = 'zrwhJfPPkSXaU';
+	$.getJSON(url,{
+		ts:ts,
+		apikey:api_key,
+		hash:hash,
+		ids:id1,
+		limit:10
+	})
+	.done(function(data){
+		console.log(data);
+		// for(var i=0;i<10;i++){
+		// 	$('#result').append(data.data.results[i]);
+		// }
+	})
+	.fail(function(err){
+		console.log(err);
+	});
+}
+
+function getAbdomenGas(){
+	var ts = new Date().getTime();
+	var hash = md5(ts+api_key).toString();
+	var url = 'https://api.giphy.com/v1/gifs';
+	var id1 = 'UAhY9fQziKNyM';
+	$.getJSON(url,{
+		ts:ts,
+		apikey:api_key,
+		hash:hash,
+		ids:id1,
+		limit:10
+	})
+	.done(function(data){
+		console.log(data);
+		// for(var i=0;i<10;i++){
+		// 	$('#result').append(data.data.results[i]);
+		// }
+	})
+	.fail(function(err){
+		console.log(err);
+	});
+}
+
+function getAbdomenTender(){
+	var ts = new Date().getTime();
+	var hash = md5(ts+api_key).toString();
+	var url = 'https://api.giphy.com/v1/gifs';
+	var id1 = '1372p1RSz94o6s';
+	$.getJSON(url,{
+		ts:ts,
+		apikey:api_key,
+		hash:hash,
+		ids:id1,
+		limit:10
+	})
+	.done(function(data){
+		console.log(data);
+		// for(var i=0;i<10;i++){
+		// 	$('#result').append(data.data.results[i]);
+		// }
+	})
+	.fail(function(err){
+		console.log(err);
+	});
+}
+
+function getHeadNormal(){
+	var ts = new Date().getTime();
+	var hash = md5(ts+api_key).toString();
+	var url = 'https://api.giphy.com/v1/gifs';
+	var id1 = '4kg8IIClzvLtC';
+	$.getJSON(url,{
+		ts:ts,
+		apikey:api_key,
+		hash:hash,
+		ids:id1,
+		limit:10
+	})
+	.done(function(data){
+		console.log(data);
+		// for(var i=0;i<10;i++){
+		// 	$('#result').append(data.data.results[i]);
+		// }
+	})
+	.fail(function(err){
+		console.log(err);
+	});
+}
+
+function getHeadDizziness(){
+	var ts = new Date().getTime();
+	var hash = md5(ts+api_key).toString();
+	var url = 'https://api.giphy.com/v1/gifs';
+	var id1 = 'xUNda89D3OJcYaMkeI';
+	$.getJSON(url,{
+		ts:ts,
+		apikey:api_key,
+		hash:hash,
+		ids:id1,
+		limit:10
+	})
+	.done(function(data){
+		console.log(data);
+		// for(var i=0;i<10;i++){
+		// 	$('#result').append(data.data.results[i]);
+		// }
+	})
+	.fail(function(err){
+		console.log(err);
+	});
+}
+
+function getHeadHeadache(){
+	var ts = new Date().getTime();
+	var hash = md5(ts+api_key).toString();
+	var url = 'https://api.giphy.com/v1/gifs';
+	var id1 = 'l378aiPPFAGEsetIkp';
+	$.getJSON(url,{
+		ts:ts,
+		apikey:api_key,
+		hash:hash,
+		ids:id1,
+		limit:10
+	})
+	.done(function(data){
+		console.log(data);
+		// for(var i=0;i<10;i++){
+		// 	$('#result').append(data.data.results[i]);
+		// }
+	})
+	.fail(function(err){
+		console.log(err);
+	});
+}
+
+function getHeadVision(){
+	var ts = new Date().getTime();
+	var hash = md5(ts+api_key).toString();
+	var url = 'https://api.giphy.com/v1/gifs';
+	var id1 = '3o6ozkchQhWRbWSTXa';
+	$.getJSON(url,{
+		ts:ts,
+		apikey:api_key,
+		hash:hash,
+		ids:id1,
+		limit:10
+	})
+	.done(function(data){
+		console.log(data);
+		// for(var i=0;i<10;i++){
+		// 	$('#result').append(data.data.results[i]);
+		// }
+	})
+	.fail(function(err){
+		console.log(err);
+	});
+}
+
+function getHeadVomiting(){
+	var ts = new Date().getTime();
+	var hash = md5(ts+api_key).toString();
+	var url = 'https://api.giphy.com/v1/gifs';
+	var id1 = 'dOl2LFw0RbTMc';
+	$.getJSON(url,{
+		ts:ts,
+		apikey:api_key,
+		hash:hash,
+		ids:id1,
+		limit:10
+	})
+	.done(function(data){
+		console.log(data);
+		// for(var i=0;i<10;i++){
+		// 	$('#result').append(data.data.results[i]);
+		// }
+	})
+	.fail(function(err){
+		console.log(err);
+	});
+}
+
+function getHeartNormal(){
+	var ts = new Date().getTime();
+	var hash = md5(ts+api_key).toString();
+	var url = 'https://api.giphy.com/v1/gifs';
+	var id1 = '3o7ZeN4rMARhZxgXqo';
+	$.getJSON(url,{
+		ts:ts,
+		apikey:api_key,
+		hash:hash,
+		ids:id1,
+		limit:10
+	})
+	.done(function(data){
+		console.log(data);
+		// for(var i=0;i<10;i++){
+		// 	$('#result').append(data.data.results[i]);
+		// }
+	})
+	.fail(function(err){
+		console.log(err);
+	});
+}
+
+function getHeartPalpitations(){
+	var ts = new Date().getTime();
+	var hash = md5(ts+api_key).toString();
+	var url = 'https://api.giphy.com/v1/gifs';
+	var id1 = '2mN50UXV2bCZW';
+	$.getJSON(url,{
+		ts:ts,
+		apikey:api_key,
+		hash:hash,
+		ids:id1,
+		limit:10
+	})
+	.done(function(data){
+		console.log(data);
+		// for(var i=0;i<10;i++){
+		// 	$('#result').append(data.data.results[i]);
+		// }
+	})
+	.fail(function(err){
+		console.log(err);
+	});
+}
+
+function getHeartMurmurs(){
+	var ts = new Date().getTime();
+	var hash = md5(ts+api_key).toString();
+	var url = 'https://api.giphy.com/v1/gifs';
+	var id1 = 'DZagytQ7EmIg';
+	$.getJSON(url,{
+		ts:ts,
+		apikey:api_key,
+		hash:hash,
+		ids:id1,
+		limit:10
+	})
+	.done(function(data){
+		console.log(data);
+		// for(var i=0;i<10;i++){
+		// 	$('#result').append(data.data.results[i]);
+		// }
+	})
+	.fail(function(err){
+		console.log(err);
+	});
+}
+
+function getHeartChestPain(){
+	var ts = new Date().getTime();
+	var hash = md5(ts+api_key).toString();
+	var url = 'https://api.giphy.com/v1/gifs';
+	var id1 = '3o6MbgOI2otYruPIkg';
+	$.getJSON(url,{
+		ts:ts,
+		apikey:api_key,
+		hash:hash,
+		ids:id1,
+		limit:10
+	})
+	.done(function(data){
+		console.log(data);
+		// for(var i=0;i<10;i++){
+		// 	$('#result').append(data.data.results[i]);
+		// }
+	})
+	.fail(function(err){
+		console.log(err);
+	});
+}
+
+function getHeartTightness(){
+	var ts = new Date().getTime();
+	var hash = md5(ts+api_key).toString();
+	var url = 'https://api.giphy.com/v1/gifs';
+	var id1 = 'nsZqyB2Qys0lG';
+	$.getJSON(url,{
+		ts:ts,
+		apikey:api_key,
+		hash:hash,
+		ids:id1,
+		limit:10
+	})
+	.done(function(data){
+		console.log(data);
+		// for(var i=0;i<10;i++){
+		// 	$('#result').append(data.data.results[i]);
+		// }
+	})
+	.fail(function(err){
+		console.log(err);
+	});
+}
+
 function showHeart(){
 	// $('#result').html("");
 	var ts = new Date().getTime();
